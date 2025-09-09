f8cd1aaa7a637b37d4f253a71cb602dadc63e75c
KentinTL <q.thibaut.lazaro96@gmail.com>
2025-09-09 13:33:05 +0200

    Create Model Patient

---
+++ patient-microservice/patient-microservice/src/main/java/com/mediscreen/patient_microservice/model/Patient.java
@@ -0,0 +1,76 @@
+package com.mediscreen.patient_microservice.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
+import lombok.Data;
+
+@Document(collection = "patients") // Indique que c'est un document MongoDB
+public class Patient {
+
+    @Id
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthdate;
+    private String gender;
+    private String address;
+    private String phoneNumber;
+}

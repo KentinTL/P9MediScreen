334ef63f8de4923e3e583526501a61cc9be33d8a
KentinTL <q.thibaut.lazaro96@gmail.com>
2025-10-07 19:44:18 +0200

    Remove getter and setter to use @Data Lombok, Remove security web token, ApplicationConfig.java SecurityConfig.java JwtAuthFilter.java JwtService.java and AuthController.java because Security make us have permission problems

--- frontend-microservice/frontend-microservice/src/main/java/com/mediscreen/frontend_microservice/dto/Patient.java
+++ frontend-microservice/frontend-microservice/src/main/java/com/mediscreen/frontend_microservice/dto/Patient.java
@@ -7,63 +7,6 @@ import lombok.Data;
 public class Patient {
     private String id;
     private String firstName;
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     private String lastName;
     private LocalDate birthdate;
     private String gender;
